73c7d54af542a1dad4d77f1435ba12f48dd95f5d
Ryan Groesch <ryan.groesch@gmail.com>
2018-07-17 16:22:49 -0700

    starting again

---
+++ automateboringstuff/hello.py
@@ -0,0 +1,11 @@
+# This program is used for introducing the hello world functionality
+
+print('Hello Programmer')
+print('What is your name?') # recieve input from user about name
+userName = input()
+print('It is nice to meet you, ' + userName)
+print('The length of your name is: ')
+print(len(userName))
+print('What is your age?') # receive input from user about age
+userAge = input()
+print('You will be ' + str(int(userAge) + 1) + ' in a year')
