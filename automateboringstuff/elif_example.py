f78a151a9268891075c9d33ecbdfd6d9115efe33
Ryan Groesch <ryan.groesch@gmail.com>
2018-07-18 17:31:03 -0700

    Chapters 2,3,4 examples

---
+++ automateboringstuff/elif_example.py
@@ -0,0 +1,10 @@
+name = 'Jimmay'
+age = 499
+if name == 'Alice':
+    print('Hello Alice')
+elif age < 26:
+    print('You are a youngin.')
+elif age > 69:
+    print('You must be Jimmay!')
+else:
+    print('I am not sure who you are, do you know?')
